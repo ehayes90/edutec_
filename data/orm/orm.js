d4498ed4f90fed0aa02e9284fe26d1b9ba8f9f70
Erik Hayes <ehayes90@outlook.com>
2016-07-19 15:36:17 -0400

    a few database changes

--- data/orm/orm.js
+++ data/orm/orm.js
@@ -1,8 +1,6 @@
-
-
 var connection = require('../database/connection.js');
 
-var seatRandom = function() {
+var getStudents = function() {
     var inquiry = 'SELECT * FROM students';
     connection.query(inquiry, function(err, data) {
         if (err) throw err;
@@ -26,13 +24,99 @@ var seatRandom = function() {
         		randSeat: randSeat
         	};
 
-        	var students = [student];
+        	var studentsArray = [student];
 
-        	console.log(students);
+        	console.log(studentsArray);
         }
     });
 };
 
-seatRandom();
+getStudents();
 
-module.exports = seatRandom;
+var getPersonnel = function() {
+    var inquiry = 'SELECT * FROM personnel';
+    connection.query(inquiry, function(err, data) {
+        if (err) throw err;
+
+        var exportPersonnel = data;
+        // console.log(data);
+
+        for (ep=0; ep<exportPersonnel.length; ep++) {
+
+            var personnelID = exportPersonnel[ep].id;
+            var personnelFN = exportPersonnel[ep].first_name;
+            var personnelLN = exportPersonnel[ep].last_name;
+            var personnelClearance = exportPersonnel[ep].clearance;
+            var personnelEmail = exportPersonnel[ep].email;
+
+            var staff = {
+                id: personnelID,
+                firstName: personnelFN,
+                lastName: personnelLN,
+                clearance: personnelClearance,
+                email: personnelEmail
+            };
+
+            var personnelArray = [staff];
+
+            console.log(personnelArray);
+
+        }
+    });
+};
+
+getPersonnel();
+
+
+
+
+
+// module.exports = seatRandom;
+
+
+// code below not to be used.. currently left for reference only.
+
+// define('connection', 'seatRandom', function(){
+
+// var connection = require('../database/connection.js');
+
+// var seatRandom = function() {
+//     var inquiry = 'SELECT * FROM students';
+//     connection.query(inquiry, function(err, data) {
+//         if (err) throw err;
+//         // console.log(data);
+
+//         var exportStudents = data;
+
+//         for (es=0; es<exportStudents.length; es++) {
+
+//             var studentID = exportStudents[es].id;
+//             var studentFN = exportStudents[es].first_name;
+//             var studentLN = exportStudents[es].last_name;
+//             var behavior = exportStudents[es].behavior;
+//             var randSeat = exportStudents[es].random_seating;
+            
+//             var student = {
+//                 id: studentID,
+//                 firstName: studentFN,
+//                 lastName: studentLN,
+//                 behavior: behavior,
+//                 randSeat: randSeat
+//             };
+
+//             var students = [student];
+
+//             console.log(students);
+//         }
+//     });
+// };
+
+// return seatRandom;
+
+// seatRandom();
+
+// // module.exports = seatRandom;
+
+// console.log('if this works');
+
+// });
--- public/scripts/auth_scripts/login.js
+++ public/scripts/auth_scripts/login.js
@@ -2,9 +2,4 @@ $(document).ready (function() {
 
 console.log('something');
 
-var orm = require('../data/orm/orm.js');
-
-seatRandom();
-
 });
-
