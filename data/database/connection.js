92ff350f8a869369a0ef0a7d72e728502f7033bf
Erik Hayes <ehayes90@outlook.com>
2016-07-23 09:21:04 -0400

    Jaws DB implementation

--- data/database/connection.js
+++ data/database/connection.js
@@ -1,5 +1,7 @@
 var mysql = require('mysql');
 
+// var JAWSDB_URL = "mysql://f8qp8z4dx56mdbce:snzplnc6c86v8p6a@nj5rh9gto1v5n05t.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/rc5weekag9d2166c";
+
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 // var connection = mysql.createConnection({
@@ -10,9 +12,17 @@ var connection = mysql.createConnection(process.env.JAWSDB_URL);
 // 	database: 'roster_db'
 // })
 
-connection.connect(function(err) {
-	if (err) throw err;
-	console.log('Connected To Database!');
+// connection.connect(function(err) {
+// 	if (err) throw err;
+// 	console.log('Connected To Database!');
+// });
+
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
 });
+
 
 module.exports = connection;
